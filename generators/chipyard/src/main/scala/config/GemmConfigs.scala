296c4f100517332730219ffc5a3401bcd7309680
shahzaibk23 <shahzaibceo@gmail.com>
2022-08-31 14:52:52 +0500

    add GEMM custom configs

---
+++ generators/chipyard/src/main/scala/config/GemmConfigs.scala
@@ -0,0 +1,14 @@
+package chipyard
+
+import freechips.rocketchip.config.{Config}
+import freechips.rocketchip.diplomacy.{AsynchronousCrossing}
+
+
+
+class GemminiCustomConfig extends Config(
+  new WithArtyTweaks ++
+  new gemmini.DefaultGemminiConfig ++                            // use Gemmini systolic array GEMM accelerator
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+
