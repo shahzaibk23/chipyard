499969b0b2a0d49d018a7be66bfbf12d00f518be
shahzaibk23 <shahzaibceo@gmail.com>
2022-09-01 19:49:23 +0500

    add QuadRocketConfig for FPGA

--- fpga/src/main/scala/arty/CustomConfigs.scala
+++ fpga/src/main/scala/arty/CustomConfigs.scala
@@ -9,3 +9,9 @@ class GemminiCustomConfig extends Config(
   new gemmini.DefaultGemminiConfig ++                        // use Gemmini systolic array GEMM accelerator
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
   new chipyard.config.AbstractConfig)
+
+class QuadRocketConfigFPGA extends Config(
+  new WithArtyTweaks ++
+  new freechips.rocketchip.subsystem.WithNBigCores(4) ++    // quad-core (4 RocketTiles)
+  new chipyard.config.AbstractConfig)
+
